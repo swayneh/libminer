3801bff09d4c9dcda1851298910e0244c5552423
Stu Hemerling <72936491+swayneh@users.noreply.github.com>
2023-08-03 14:01:58 -0700

    add tests for lib_summary

---
+++ tests/testthat/test-lib-summary.R
@@ -0,0 +1,15 @@
+test_that("lib_summary returns expected results", {
+  result <- lib_summary()
+  expect_s3_class(result, "data.frame") # should be a dataframe
+  expect_equal(ncol(result), 2) # should return 2 columns
+  expect_equal(names(result), "library", "n_packages")
+  expect_type(result$library, "character")
+  expect_type(result$n_packages, "double")
+  expect_gt(sum(result$n_packages), 0)
+  expect_equivalent(sort(results$library), sort(.libPaths()))
+  expect_setequal(results$library, libPath)
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), )
+})
